c9da931fbe68c1fa5b5c0f60ac555c448ff59bb1
Zongwen Li <zwli@thoughtworks.com>
2020-11-27 14:32:45 +0800

    fix error

--- src/main/java/com/lzw/niodemo/NioDemoApplication.java
+++ src/main/java/com/lzw/niodemo/NioDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NioDemoApplication {
-sfasdft
     public static void main(String[] args) {
         SpringApplication.run(NioDemoApplication.class, args);
     }
-
 }
